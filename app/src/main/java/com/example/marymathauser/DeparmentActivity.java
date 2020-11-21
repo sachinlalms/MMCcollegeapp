c464360f1cda48aea669cf8dac080b356a8fd2c5
sachinlalms <sachinlalms95@gmail.com>
2020-11-21 22:24:45 +0530

    second commit

---
+++ app/src/main/java/com/example/marymathauser/DeparmentActivity.java
@@ -0,0 +1,14 @@
+package com.example.marymathauser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeparmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deparment);
+    }
+}
---
+++ app/src/main/java/com/example/marymathauser/qpapper/QuestionAdapter.java
@@ -0,0 +1,4 @@
+package com.example.marymathauser.qpapper;
+
+public class QuestionAdapter {
+}
